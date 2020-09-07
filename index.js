76e1ecd69b661c023e693884f5ec8ffa16c61d37
simsim1324 <65931804+simsim1324@users.noreply.github.com>
2020-09-07 09:56:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const request = require('request');
 const fs = require('fs');
 const client = new Discord.Client();
 const token = process.env.token;
-const welcomeChannelName = "노는방";
-const byeChannelName = "안녕히가세요";
+const welcomeChannelName = "봇-채널";
+const byeChannelName = "봇-채널";
 const welcomeChannelComment = "환영합니다! s?로 제 명령어를 확인해보세요!!";
 const byeChannelComment = "다음에 또 봐요...";
 let PREFIX = 's! ';
@@ -33,16 +33,6 @@ client.on("guildMemberRemove", (member) => {
 
 client.on('message', (message) => {
     
-  if (message.content === '파일테스트') {
-    fs.writeFileSync('test/test.js', 'utf-8', 'test');
-    message.channel.send('테스트 성공!');
-  }
-   
-  if (message.content === '파일읽기') {
-    let l = fs.readFileSync('test/test.js', 'utf-8');
-    message.channel.send(l+'\n\n읽기 성공!');
-  }
-   
  if(message.content == 's?') {
     let helpImg = 'https://cdn.discordapp.com/attachments/714828023093788752/715066220868534332/KakaoTalk_20200523_212814624.jpg';
     let embed = new Discord.RichEmbed()
@@ -52,15 +42,15 @@ client.on('message', (message) => {
       .setTimestamp()
 
     embed.addField('Command: ', '• `s?`: 도움말이 뜹니다.');
-    embed.addField('General: ', '• `안녕`: 인사를 합니다.');
+    embed.addField('General: ', '• `테스트`: 메세지 변경을 테스트 합니다.');
     embed.addField('Game: ', '• `제작중`: 곧 공개됩니다...');
     embed.addField('Bot: ', '• `심심아`: 심심이가 대답합니다.');
 
     message.channel.send(embed)
   }
 
-  if (message.content === '안녕') {
-    message.channel.send('반가워');
+  if (message.content === '테스트') {
+    editMsg(message, "테스트 5초 뒤 변경", "테스트 변경완료!", 5000);
   }
 
   if (message.content === '심심아') {
@@ -71,7 +61,7 @@ client.on('message', (message) => {
 });
 
 
-async function editMsg(message, str1, str2, delay = 2000) {
+async function editMsg(message, str1, str2, delay) {
   let msg = await message.channel.send(str1);
 
   setTimeout(() => {
