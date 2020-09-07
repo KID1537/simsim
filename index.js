70ad7cfec5c59fe70317a651315e920292733cb9
simsim1324 <65931804+simsim1324@users.noreply.github.com>
2020-09-07 18:00:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -30,36 +30,36 @@ client.on("guildMemberRemove", (member) => {
 
 client.on('message', (message) => {
 	if(message.content == 's?') {
-      	let helpImg = 'https://simsim.msub.kr/img/simsim.jpg';
-      	let embed = new Discord.RichEmbed()
-         	.setAuthor('SIMSIM Commands', helpImg)
-         	.setColor('#186de6')
-         	.setFooter(`SIMSIM`)
-         	.setTimestamp()
-         	.addField('Command: ', '• `s?`: 도움말이 뜹니다.');
-         	.addField('General: ', '• `s뉴스`: 실시간 네이버 뉴스 top10을 보냅니다.');
-         	.addField('Game: ', '• `제작중`: 곧 공개됩니다...');
-         	.addField('Bot: ', '• `심심아`: 심심이가 대답합니다.');
-      	message.channel.send(embed);
+      		let helpImg = 'https://simsim.msub.kr/img/simsim.jpg';
+      		let embed = new Discord.RichEmbed()
+         		.setAuthor('SIMSIM Commands', helpImg)
+         		.setColor('#186de6')
+         		.setFooter(`SIMSIM`)
+         		.setTimestamp()
+         		.addField('Command: ', '• `s?`: 도움말이 뜹니다.');
+         		.addField('General: ', '• `s뉴스`: 실시간 네이버 뉴스 top10을 보냅니다.');
+         		.addField('Game: ', '• `제작중`: 곧 공개됩니다...');
+         		.addField('Bot: ', '• `심심아`: 심심이가 대답합니다.');
+      		message.channel.send(embed);
    	}
 
    	if (message.content === '테스트') {
-      	editMsg(message, "테스트 5초 뒤 변경", "테스트 변경완료!", 5000);
+      		editMsg(message, "테스트 5초 뒤 변경", "테스트 변경완료!", 5000);
    	}
 
    	if (message.content === '심심아') {
-      	let reply_1 = ['부르셨어요?','왜요?','혹시 찾으셨나요?'];
-      	message.reply(reply_1[Math.floor(Math.random()*3)]);
+      		let reply_1 = ['부르셨어요?','왜요?','혹시 찾으셨나요?'];
+      		message.reply(reply_1[Math.floor(Math.random()*3)]);
    	}
     
-    if (message.content === 's뉴스') {
-        request.get('http://simsim.msub.kr/api/news/', function (error, response, body) {
-        	let helpImg = 'https://simsim.msub.kr/img/simsim.jpg';
-        	let embed = new Discord.RichEmbed()
-        		.setAuthor('SIMSIM Commands', helpImg)
-      			.setColor('#186de6')
-      			.setFooter(`SIMSIM`)
-      			.setTimestamp()
+    	if (message.content === 's뉴스') {
+        	request.get('http://simsim.msub.kr/api/news/', function (error, response, body) {
+        		let helpImg = 'https://simsim.msub.kr/img/simsim.jpg';
+        		let embed = new Discord.RichEmbed()
+        			.setAuthor('SIMSIM Commands', helpImg)
+      				.setColor('#186de6')
+      				.setFooter(`SIMSIM`)
+      				.setTimestamp()
 				.addField('실시간 뉴스 top 10!', '\n'+body);
 			message.channel.send(embed)
    		});
@@ -67,11 +67,11 @@ client.on('message', (message) => {
    
    	if (message.content === 's한강') {
    		request.get('https://hangang.msub.kr/', function (error, response, body) {
-        	var $ = cheerio.load(body);
-      		$('#temp').each(function(){
-    			var temp = $(this);
-    			var temp_text = temp.text(); 
-     			message.channel.send(temp);
+        		var $ = cheerio.load(body);
+      			$('#temp').each(function(){
+    				var temp = $(this);
+    				var temp_text = temp.text(); 
+     				message.channel.send(temp);
    			});
    		});
    	}
@@ -80,11 +80,11 @@ client.on('message', (message) => {
 
 
 async function editMsg(message, str1, str2, delay) {
-  let msg = await message.channel.send(str1);
+	let msg = await message.channel.send(str1);
 
-  setTimeout(() => {
-    msg.edit(str2);
-  }, delay);
+	setTimeout(() => {
+    	msg.edit(str2);
+  	}, delay);
 }
 
 client.login(token);
